7af22dffe6c0506eab15ac17d1827469f721542f
nattinats <n.szajner@gmail.com>
2022-02-16 14:31:13 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName('mobile-hamburger')[0].addEventListener('click', function () {
+  document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+
+});
+
+
+document.getElementsByClassName('mobile-close')[0].addEventListener('click', function () {
+  document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+
+})
